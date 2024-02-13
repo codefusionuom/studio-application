d288ad8b7ce5d493df62d90fc9dd7bcff4c5abe7
Yasith_99 <yasithsarada79@gmail.com>
2024-02-13 17:22:55 +0530

    routes done

--- src/App.js
+++ src/App.js
@@ -39,10 +39,10 @@ function App() {
       </Route>
 
       <Route path="/eventManager" element={<EventManager/>}>
-        {/* <Route path="/eventManager/" element={<EventManagerDashboard/>}/>
+        <Route path="/eventManager/" element={<EventManagerDashboard/>}/>
         <Route path="/eventManager/eventRequests" element={<EventRequests />}/>
         <Route path="/eventManager/eventCalandar" element={<EventManagerEventCalendar/>} />
-        <Route path="/eventManager/Events" element={<EventManagerEvents/>} /> */}
+        <Route path="/eventManager/Events" element={<EventManagerEvents/>} />
       </Route>
       
       <Route path="/stockManager" element={<StockManager />}>
