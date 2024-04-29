951ba7b147130fa331548a8d77e06e3a5dc0f947
Yasith_99 <yasithsarada79@gmail.com>
2024-04-29 18:51:38 +0530

    try

--- src/pages/eventManager/createEvent.js
+++ src/pages/eventManager/createEvent.js
@@ -9,7 +9,7 @@ import {
   PopoverHandler,
   Popover,
   Input,
-  Button,
+  Button,//
 } from "@material-tailwind/react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/outline";
 import { DayPicker } from "react-day-picker";
