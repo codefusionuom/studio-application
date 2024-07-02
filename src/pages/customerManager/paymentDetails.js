359f29d0751aa281e243bc3c2219773ed677f9fc
Yasith_99 <yasithsarada79@gmail.com>
2024-07-03 03:05:49 +0530

    merged with custormer manager

--- src/pages/customerManager/paymentDetails.js
+++ src/pages/customerManager/paymentDetails.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axiosInstance from "../../config/axios.config";
-import BackButton from "../../components/buttons/backButton";
+import BackButton from "../../components/buttons/BackButton";
 
 
 function PaymentDetails() {
